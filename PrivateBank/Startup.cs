e4bb6323b32c04e1fb6e8d0fb793877c107a1e45
ASIF <ASIF@DESKTOP-I3HL0G4>
2017-07-30 01:12:13 +0600

    Add project files.

---
+++ PrivateBank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrivateBank.Startup))]
+namespace PrivateBank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
